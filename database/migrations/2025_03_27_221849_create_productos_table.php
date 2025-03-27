0ef4ec69a43351db084d715fcf3f4532200f7d4a
Ingrid Rosales <nahocarlos@gmail.com>
2025-03-27 17:44:05 -0500

    nuevo

---
+++ database/migrations/2025_03_27_221849_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->float('precio', 20);
+            $table->string('categoria', 100);
+            $table->integer('stock', 30);
+            $table->string('detalle', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
